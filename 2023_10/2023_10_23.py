24d154d3ed7a1501bab1f786c9161c1f0e741833
Rinevard <136295900+rinevard@users.noreply.github.com>
2023-10-23 12:46:26 +0800

    Add files via upload

--- 2023_10/2023_10_23.py
+++ 2023_10/2023_10_23.py
@@ -1,10 +1,22 @@
-# 2023/10/23 Daily challenge
-# https://leetcode.com/problems/power-of-four/
+# 2023/10/22 Daily challenge
+# https://leetcode.com/problems/maximum-score-of-a-good-subarray/
 
 class Solution:
-    def isPowerOfFour(self, n: int) -> bool:
-        if n <= 0: return False
+    def maximumScore(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        min_val = nums[k]
+        left, right = k, k
+        ans = 0
 
-        # (n & n - 1) == 0 判断一个数是否是2的幂次，
-        # (2^(2k+1) - 1) % 3 == 1, (2^(2k) - 1) % 3 == (4^k - 1) % 3 == 0
-        return (n & n - 1) == 0 and (n - 1) % 3 == 0
+        # 注意这样一个事实：假设我们在(i, j)处取得最大分数，那么min(nums[i:j+1])一定大于nums[i-1]和nums[j+1]
+        # 否则，我们可以令i = i - 1或者令j = j + 1
+        while left >= 0 or right < n:
+            while left >= 0 and nums[left] >= min_val:
+                left -= 1
+            while right < n and nums[right]>= min_val:
+                right += 1
+
+            ans = max(ans, (right - left + 1 - 2) * min_val)
+            min_val = max(nums[left] if left >= 0 else 0, nums[right] if right < n else 0)
+       
+        return ans
