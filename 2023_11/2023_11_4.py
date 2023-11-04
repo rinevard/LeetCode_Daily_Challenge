c0127d03fc6ae92f277841cbebf5226e51e2bf53
Rinevard <136295900+rinevard@users.noreply.github.com>
2023-11-04 12:47:08 +0800

    Update 2023_11_4.py

--- 2023_11/2023_11_4.py
+++ 2023_11/2023_11_4.py
@@ -1,6 +1,7 @@
 # 2023/11/4 Daily challenge
 # https://leetcode.com/problems/last-moment-before-all-ants-fall-out-of-a-plank/
 
+# 把相遇的蚂蚁互换编号，相当于两只蚂蚁穿过了对方。
 class Solution:
     def getLastMoment(self, n: int, left: List[int], right: List[int]) -> int:
         return max([i for i in left]+[n - j for j in right])
